bf11d6ead4e033713cf51f458383887b4a41fba5
OmarAlhalaby <vip.omar93@gmail.com>
2023-11-18 11:50:37 +0800

    initiate

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import TheGame from './components/TheGame.vue'
+</script>
+
+<template>
+  <main>
+    <TheGame />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheGame.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Cards Game
+  </div>
+</template>
